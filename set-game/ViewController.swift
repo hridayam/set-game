bd99160f7a2b3927491d90894802b1ca88970fdf
hridayam <hridayambakshi2011@gmail.com>
2018-03-04 22:52:41 -0800

    set game lab-1 correct version

--- set-game/ViewController.swift
+++ set-game/ViewController.swift
@@ -2,8 +2,9 @@
 //  ViewController.swift
 //  set-game
 //
-//  Created by Rooh on 2/9/18.
-//  Copyright © 2018 Raghav Gupta. All rights reserved.
+//  Created by hridayam bakshi on 2/9/18.
+//  Copyright © 2018 hridayam bakshi. All rights reserved.
+//
 
 import UIKit
 
@@ -11,7 +12,7 @@ class ViewController: UIViewController {
     
     //initializing variables
     var game: Game = Game()
-    //var cardModel: CardsModel = CardsModel()
+    var cardModel: CardsModel = CardsModel()
     var selectedButtons: [Int] = []
     var score = 0 {
         didSet {
@@ -107,8 +108,8 @@ class ViewController: UIViewController {
                 var attributes: [NSAttributedStringKey: Any]
                 var stroke: Int
                 
-                for _ in 0..<game.getCard(index: i)!.count {
-                    temp.append(game.getCard(index: i)!.character)
+                for _ in 0..<game.getCard(index: i)!.count.rawValue {
+                    temp.append(game.getCard(index: i)!.symbol.rawValue)
                 }
                 
                 if temp.count == 2 {
@@ -118,10 +119,10 @@ class ViewController: UIViewController {
                     cardButtons[i].titleLabel?.font = cardButtons[i].titleLabel?.font.withSize(29)
                 }
                 
-                if game.getCard(index: i)!.color == "green" {
+                if game.getCard(index: i)!.color == Color.green {
                     color = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
-                } else if game.getCard(index: i)!.color == "blue" {
-                    color = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+                } else if game.getCard(index: i)!.color == Color.purple {
+                    color = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
                 } else {
                     color = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
                 }
